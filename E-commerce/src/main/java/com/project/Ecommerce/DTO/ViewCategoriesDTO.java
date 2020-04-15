985366fe2e05f403b557ffd88803c31abaff7ed9
pinki <pinki.bhati@tothenew.com>
2020-04-15 17:44:24 +0530

    created ViewCategoriesDTO

---
+++ E-commerce/src/main/java/com/project/Ecommerce/DTO/ViewCategoriesDTO.java
@@ -0,0 +1,38 @@
+package com.project.Ecommerce.DTO;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import java.util.List;
+
+@Component
+public class ViewCategoriesDTO
+{
+    String name;
+    List<String> fieldName;
+    List<String> values;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(List<String> fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public List<String> getValues() {
+        return values;
+    }
+
+    public void setValues(List<String> values) {
+        this.values = values;
+    }
+}
